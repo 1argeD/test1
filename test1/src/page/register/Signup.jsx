54208f8ef6a57615e0672e92e57257f0c4c138bb
1argeD <gkswhdgur1234@naver.com>
2022-11-23 19:33:11 +0900

    Main

--- test1/src/page/register/Signup.jsx
+++ test1/src/page/register/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { useFrom, useWatch } from "react-hook-form"
+import { appendErrors, useFrom, useWatch } from "react-hook-form"
 import { useDispatch, useSelector } from "react-redux";
 import { 
     existMemberId, registerUser, existMemberNickname 
@@ -32,7 +32,7 @@ const SignupForm = () => {
         reset,
         setValue,
         control,
-        formState: { isDirty, error },
+        formState: { isDirty, errors },
     } = useFrom({mode:"onChange", userId: "text"});
 
     const onSumbit = (event) => {
@@ -46,7 +46,7 @@ const SignupForm = () => {
             password: watch().password,
             passwordConfirm: watch().passwordConfirm,
         };
-        if (isDoubleCheck && nickDoubleCheck) {
+        if (idDoubleCheck && nickDoubleCheck) {
             dispatch(registerUser(body));
         }
     }
@@ -118,6 +118,141 @@ const SignupForm = () => {
     
     return (
         <>
+            {!registerSuccess && (
+                <GlobalModal
+                    content1={`아이디 혹은 닉네임 중복확인 바랍니다.`}
+                    isModal={idDuplicate}
+                    setIsModal={onIdDuplicate}
+                />
+            )}
+            {idDuplicate && !nickDuplicate && idSuccess && (
+                <GlobalModal
+                    content1={`사용이 가능한 아이디 입니다.`}
+                    isModal={idDuplicate}
+                    setIsModal={onIdDuplicate}
+                />
+            )}
+            {idDuplicate && !nickDuplicate && !idSuccess && (
+                <GlobalModal
+                    content1={`이미 존재하는 아이디 입니다.`}
+                    isModal={idDuplicate}
+                    setIsModal={onIdDuplicate}
+                />
+            )}
+            {!idDuplicate && nickDuplicate && nickSuccess && (
+                <GlobalModal
+                    content1={`사용 가능한 닉네임 입니다.`}
+                    isModal={nickDuplicate}
+                    setIsModal={onNickDuplicate}
+                />
+            )}
+            {!idDuplicate && nickDuplicate && !nickDuplicate && (
+                <GlobalModal
+                    content1={`이미 존재하는 닉네임입니다.`}
+                    isModal={nickDuplicate}
+                    setIsModal={onNickDuplicate}
+                />
+            )}
+            <Form 
+                className="signup-form"
+                onSumbit={handleSubmit(onSumbit,onError,onDuplicateUserId)}
+                >
+                    <TitleWrapper>
+                        <Title>회원가입</Title>
+                    </TitleWrapper>
+                    <Comtatiner>
+                        <Label>아이디</Label>
+                    </Comtatiner>
+                    <inputWrapper>
+                        <Input
+                            type="password"
+                            tabIndex="2"
+                            className="input"
+                            {...register("password",{
+                                required:"비밀번호는 숫자, 문자 조합",
+                                pattern: {
+                                    value: /^[A-Za-z0-9]{6,12}$/,
+                                    message: "비밀번호는 숫자, 문자 조합 6자 이상 적어주세요.",
+                                },
+                            })}
+                            aria-invalid={
+                                !isDirty ? undefined : appendErrors.password ? "true" : "false"
+                            }
+                            name="password"
+                            />
+                            {appendErrors.password && (
+                                <HelperText2>{errors?.password?.message}</HelperText2>
+                            )}
+
+                            {!errors.password && (
+                                <HelperText>
+                                    {"비밀번호는 숫자, 문자 조합 6자 이상 적어주세요"}
+                                </HelperText>
+                            )}
+                    </inputWrapper>
+
+                    <Label>비밀번호 확인</Label>
+                    <inputWrapper>
+                        <Input
+                            type="password"
+                            className="input"
+                            tabIndex="2"
+                            {...register("passwordconfirm", {
+                                required:"비밀번호는 숫자, 문자 조합 6자 이상 적어주세요",
+                                pattern: {
+                                    value: /^[A-Za-z0-9]{6,12}$/,
+                                    message: "비밀번호는 숫자, 문자 조합 6자 이상 적어주세요.",
+                                },
+                            })}
+                            aria-invalid={
+                                !isDirty
+                                    ? undefined
+                                    : errors.passwordConfirm
+                                    ? "true"
+                                    : "false"
+                            }
+                            name="passwordConfirm"
+                        />
+                        {errors.passwordConfirm && (
+                            <HelperText2>{errors?.passwordConfirm?.message}</HelperText2>
+                        )}
+                        {!errors.passwordConfirm && (
+                            <HelperText>
+                                {"비밀번호는 숫자, 문자 조합 6자 이상 적어주세요."}
+                            </HelperText>
+                        )}
+                    </inputWrapper>
+                    
+                    <Label>닉네임</Label>
+                    <inputWrapper>
+                            <Input
+                                type="text"
+                                className="input"
+                                tabIndex="2"
+                                onChange={() => onNickDuplicate(false)}
+                                {...register("nickname", {
+                                    required: "사용할 닉네임을 적어주세요."
+                                })}
+                                aria-invalid={
+                                    !isDirty ? undefined : errors.nickName ? "ture" : "false"
+                                }
+                                name="nickname"
+                            />
+                            <ButtonWrapper>
+                                <Button
+                                content={"중복체크"}
+                                className="nickcheck-btn"
+                                onClick={(event) => {
+                                    event.prevenDfault();
+                                    event.stopPropagation();
+                                    onDuplicateUserNicknmae();
+                                    onNickDuplicate((prev) => ! prev);
+                                    onNickDoubleCheck(true)
+                                }}
+                                ></Button>
+                            </ButtonWrapper>
+                    </inputWrapper>
+            </Form>
         </>
     )
 
